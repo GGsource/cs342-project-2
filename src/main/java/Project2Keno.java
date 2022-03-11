e5eda3d1212da11edd9daf8d699ee9cf17a04050
ggonza55 <ggonza55@uic.edu>
2022-03-11 13:51:31 -0600

    uploaded logo and gitignore

--- src/main/java/Project2Keno.java
+++ src/main/java/Project2Keno.java
@@ -1,7 +1,10 @@
 import javafx.application.Application;
 
 import javafx.scene.Scene;
-
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -16,10 +19,27 @@ public class Project2Keno extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Welcome to JavaFX");
 		
+		//A menubar holds a series of menus
+		//A menu holds a series of menuitems
+		//A menuitem functions as a button and can have an event handler
+		MenuItem rulesMenuItem = new MenuItem("Rules");
+		//TODO: Implement action for rules menu item being clicked
+		rulesMenuItem.setOnAction(e->System.out.println("We have yet to implement rules menu item functionality!"));
+		MenuItem oddsMenuItem = new MenuItem("Odds");
+		//TODO: Implement action for odds menu item being clicked
+		oddsMenuItem.setOnAction(e->System.out.println("We have yet to implement odds menu item functionality!"));
+		
+		Menu mainMenu = new Menu("Menu");
+		mainMenu.getItems().addAll(rulesMenuItem, oddsMenuItem); //Add items to menu
+		MenuBar welcomeMenuBar = new MenuBar(mainMenu);
+
+		//Add the main logo to the welcome screen
+		Image kenoImage = new Image("/src/main/resources/Keno_Logo.png", true);
+		
 		
 		
 				
-		Scene scene = new Scene(new VBox(), 700,700);
+		Scene scene = new Scene(welcomeMenuBar, 700,700);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
