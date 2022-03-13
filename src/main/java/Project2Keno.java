e3b1c2faed145013a7de344f3f492f653fc6e7ce
GGPC <ggonza55@uic.edu>
2022-03-13 16:08:24 -0500

    pickForMe implemented & gridReset implemented.

--- src/main/java/Project2Keno.java
+++ src/main/java/Project2Keno.java
@@ -1,8 +1,8 @@
 
 import java.util.HashSet;
+import java.util.Random;
 
 import javafx.application.Application;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -33,7 +33,7 @@ public class Project2Keno extends Application {
 	private int currentSpotCount;//Amount of spots user has chosen so far
 	private Button chosenSpotButton = null; //Keep track of which spot button is chosen
 	private int drawCount;
-	private HashSet<Button> spotsSet = new HashSet<>();
+	private HashSet<ToggleButton> spotsSet = new HashSet<>();
 	
 	private void initializeScenes(Stage givenStage) {
 		welcomeScene = createWelcomeScene(givenStage);
@@ -125,44 +125,50 @@ public class Project2Keno extends Application {
 				int currentNum = (i*10)+j+1; //This is the correct number displayed on button
 				ToggleButton gridButton = new ToggleButton(""+ currentNum);
 				//Color gridbutton
-				gridButton.setStyle("-fx-background-color: #b0d57b");
+				gridButton.setStyle("-fx-background-color: " + ToggleButton.normalColor);
 				//Give the buttons behavior on press
 				gridButton.setOnAction(e->{
-					if (gridButton.isSelected()) { //User pressed it again so they must want to unpick this number
-						//set it back to unpressed
-						gridButton.setSelected(false);
-						//Set color back to normal
-						gridButton.setStyle("-fx-background-color: #b0d57b");
-						//decrement amount of buttons selected by 1
-						currentSpotCount -= 1;
-						//Remove from list of chosen numbers
-						spotsSet.remove(gridButton);
-						//DEBUGGING: Line to be disabled later
-						System.out.println("Currently Chosen Spots: " + spotsSet);
-						//number of buttons selected is now LESS than chosenSpotCount, reenable all buttons
-						for (Node gButton : betCardGridPane.getChildren()) {
-							ToggleButton castedBtn = (ToggleButton) gButton;
-							castedBtn.setDisable(false);
+					//If we already have enough spots and a new button is pressed, it should be
+					//Ignored unless it is one of the already chosen buttons, in which case it
+					//will be unchosen.
+					if ((currentSpotCount < chosenSpotCount) || spotsSet.contains(gridButton))
+					{
+						if (gridButton.isSelected()) { //User pressed it again so they must want to unpick this number
+							//set it back to unpressed
+							gridButton.setSelected(false);
+							//Set color back to normal
+							gridButton.setStyle("-fx-background-color: " + ToggleButton.normalColor);
+							//decrement amount of buttons selected by 1
+							currentSpotCount -= 1;
+							//Remove from list of chosen numbers
+							spotsSet.remove(gridButton);
+							//DEBUGGING: Line to be disabled later
+							System.out.println("Currently Chosen Spots: " + spotsSet);
+							//number of buttons selected is now LESS than chosenSpotCount, reenable all buttons
+							// for (Node gButton : betCardGridPane.getChildren()) {
+							// 	ToggleButton castedBtn = (ToggleButton) gButton;
+							// 	castedBtn.setDisable(false);
+							// }
 						}
-					}
-					else {
-						gridButton.setSelected(true);//Set it to pressed so we can toggle
-						//Change color to stand out
-						gridButton.setStyle("-fx-background-color: #9bd747");
-						//Increment amount of buttons selected by 1
-						currentSpotCount += 1;
-						//Add to list of chosen numbers
-						spotsSet.add(gridButton);
-						//DEBUGGING: line to be disabled later
-						System.out.println("Currently Chosen Spots: " + spotsSet);
-						//If amount of buttons selected is now = to chosenSpotCount then disable all buttons OTHER than already chosen ones
-						if (currentSpotCount >= chosenSpotCount) {
-							for (Node gButton : betCardGridPane.getChildren()) {
-								ToggleButton castedBtn = (ToggleButton) gButton; //have to recast to access it
-								if (!spotsSet.contains(castedBtn)) { //If the button is not in our set
-									gButton.setDisable(true); //Disable it
-								}
-							}
+						else {
+							gridButton.setSelected(true);//Set it to pressed so we can toggle
+							//Change color to stand out
+							gridButton.setStyle("-fx-background-color: " + ToggleButton.highlightedColor);
+							//Increment amount of buttons selected by 1
+							currentSpotCount += 1;
+							//Add to list of chosen numbers
+							spotsSet.add(gridButton);
+							//DEBUGGING: line to be disabled later
+							System.out.println("Currently Chosen Spots: " + spotsSet);
+							//If amount of buttons selected is now = to chosenSpotCount then disable all buttons OTHER than already chosen ones
+							// if (currentSpotCount >= chosenSpotCount) {
+							// 	for (Node gButton : betCardGridPane.getChildren()) {
+							// 		ToggleButton castedBtn = (ToggleButton) gButton; //have to recast to access it
+							// 		if (!spotsSet.contains(castedBtn)) { //If the button is not in our set
+							// 			gButton.setDisable(true); //Disable it
+							// 		}
+							// 	}
+							// }
 						}
 					}
 				});
@@ -174,7 +180,7 @@ public class Project2Keno extends Application {
 		//Pick for Me Button
 		Button pickForMeButton = new Button("Pick For Me!");
 		//Make button pick random nums
-		pickForMeButton.setOnAction(e->randomizeBetCard());
+		pickForMeButton.setOnAction(e->pickBetCardForMe(betCardGridPane));
 		//Disable until spot count is chosen
 		pickForMeButton.setDisable(true);
 
@@ -186,15 +192,15 @@ public class Project2Keno extends Application {
 		Button spotButtonList[] = {new Button("1"), new Button("4"), new Button("8"), new Button("10")};
 		for (Button spotButton : spotButtonList) {
 			spotsButtonsHBox.getChildren().add(spotButton); //Add the button to HBox
-			spotButton.setStyle("-fx-background-color: #e87564;");
+			spotButton.setStyle("-fx-background-color: #e87564");
 			//Pressing these buttons sets spotCount to the intended Number
 			spotButton.setOnAction(e->{
 				if (chosenSpotButton != spotButton) { //No need to repeatedly set if it's already set
 					//Make the pressed button stand out
-					spotButton.setStyle("-fx-background-color: #e24028;");
+					spotButton.setStyle("-fx-background-color: #e24028");
 					//make sure any previous pressed are returned to normal
 					if (chosenSpotButton != null)
-						chosenSpotButton.setStyle("-fx-background-color: #e87564;");
+						chosenSpotButton.setStyle("-fx-background-color: #e87564");
 					chosenSpotButton = spotButton; //update chosen spot button
 					chosenSpotCount = Integer.parseInt(spotButton.getText());
 					//DEBUGGING:
@@ -203,7 +209,8 @@ public class Project2Keno extends Application {
 					betCardGridPane.setDisable(false);
 					//Enable random pick too
 					pickForMeButton.setDisable(false);
-					//TODO: Make it so picking a spotcount clears the gridpane selections
+					//DONE: Make it so picking a spotcount clears the gridpane selections
+					resetGrid(betCardGridPane);
 					//pickforme should also clear gridpane. make it a function
 				}
 			});
@@ -259,9 +266,49 @@ public class Project2Keno extends Application {
 		System.out.println("User wants to change the look of the application but this has not yet been implemented...");
 	}
 
-	private void randomizeBetCard() {
-		//TODO: Implement Randomized selection of bet card
-		System.out.println("Randomize Bet Card was chosen but is not implemented yet...");
+	//DONE: Randomized selection of bet card
+	private void pickBetCardForMe(GridPane givenGridPane) {
+		//should reset grid before picking
+		resetGrid(givenGridPane);
+		//Create a Random Object to create numbers
+		Random generatorRandom = new Random();
+		//Pick chosenSpotCount amount of random numbers
+		for (int i = 0; i < chosenSpotCount; i++) {
+			int randomSpot = generatorRandom.nextInt(79) + 1; //Plus 1 since random starts at 0
+			ToggleButton randomSpotButton = (ToggleButton)givenGridPane.getChildren().get(randomSpot);
+			//Check if this is a repeat
+			if (spotsSet.contains(randomSpotButton)) {
+				//DEBUGGING:
+				System.out.println("DUPLICATE RANDOM GIVEN! Trying for another number.");
+				i--;//Make sure we still run enough times even if there's a duplicate
+				continue;//Don't run the rest of the code, start a new iteration of the loop
+			}
+			//Set button as selected
+			randomSpotButton.setSelected(true);
+			//Show it has been chosen visibly
+			randomSpotButton.setStyle("-fx-background-color: " + ToggleButton.highlightedColor);
+			//increment currentSpotCount
+			currentSpotCount += 1;
+			//Add the button to our set
+			spotsSet.add(randomSpotButton);
+		}
+	}
+
+	private void resetGrid(GridPane givenGridPane) {
+		if (spotsSet.isEmpty())
+			return; //No spots have been selected, there's no need to reset anything
+		//DEBUGGING:
+		System.out.println("resetGrid was called successfully");
+		for (ToggleButton spotButton : spotsSet) {
+			//We need to reset button color
+			spotButton.setStyle("-fx-background-color: " + ToggleButton.normalColor);
+			//We need to set button back to unpressed
+			spotButton.setSelected(false);
+		}
+		//We need to clear out the spotSet
+		spotsSet.clear();
+		//We need to reset the currentSpotCount
+		currentSpotCount = 0;
 	}
 
 }
